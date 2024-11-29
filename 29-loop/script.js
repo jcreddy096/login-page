a74f5d81925b88811d216b0e879ec1be274dbae5
jcreddy096 <jcreddyvennapusa@gmail.com>
2024-11-29 21:38:09 +0530

    upload:loop

---
+++ 29-loop/script.js
@@ -0,0 +1,58 @@
+for (let i = 1; i <=10; i++)
+{console.log(i)};
+
+
+let fruits = ["apple","banana","orange","mango"]
+for(let a = 0; a < fruits.length; a++)
+{console.log(fruits[a]);};
+
+for (let b = 1; b <=3; b++)
+    {console.log(b)
+        for(let c = 1; c <= 2; c++ )
+        {console.log("jcreddy",+c)}
+    };
+
+
+    let i = 0;
+while (i < 5) {
+    console.log(i);
+    i++;
+};
+
+
+let x = 10;
+do {
+    console.log(x);
+    x++;
+} while (x < 20);
+
+
+let y = 10;
+do {
+    console.log(y);
+    i++;
+} while (y < 5);
+
+
+
+for (let i = 0; i < 5; i++) {
+    if (i === 3) {
+        break; 
+    }
+    console.log(i);
+};
+
+
+
+
+for (let i = 0; i < 5; i++) {
+    if (i === 3) {
+        continue; 
+    }
+    console.log(i);
+}
+
+
+
+
+    
